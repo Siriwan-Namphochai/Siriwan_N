6a266024f9ed72c8ebc27ab1572441a28d3df1bb
Siriwan N <163093503+Siriwan-Namphochai@users.noreply.github.com>
2025-08-05 08:36:34 +0000

    ทดส Push

---
+++ Backend/main.go
@@ -0,0 +1,26 @@
+package main
+
+
+import (
+    "siriwan-backend/controller"
+    "siriwan-backend/entity"
+)
+
+
+func main() {
+  entity.SetupDatabase()
+
+
+  r := gin.Default()
+
+
+  // User Routes
+  r.GET("/users", controller.ListUsers)
+  r.GET("/user/:id", controller.GetUser)
+  r.POST("/users", controller.CreateUser)
+  r.PATCH("/users", controller.UpdateUser)
+  r.DELETE("/users/:id", controller.DeleteUser)
+  
+  // Run the server
+  r.Run()
+}
